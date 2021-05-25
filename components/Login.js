786b70c4db98ef7f56d8e022dd5b3c09163ff630
VishalUrankar-2211 <vvu060@gmail.com>
2021-05-25 12:34:24 +0530

    Next auth url changed

--- components/Login.js
+++ components/Login.js
@@ -15,7 +15,7 @@ function Login() {
         onClick={signIn}
         className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
       >
-        Login with Facebook
+        Login with Gmail
       </h1>
     </div>
   );
