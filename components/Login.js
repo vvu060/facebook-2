3c3ce83f115ad42ed78b0087356a05c3a161baa5
VishalUrankar-2211 <vvu060@gmail.com>
2021-05-25 12:45:06 +0530

    FB added back

--- components/Login.js
+++ components/Login.js
@@ -15,7 +15,7 @@ function Login() {
         onClick={signIn}
         className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
       >
-        Login with Gmail
+        Login with Facebook
       </h1>
     </div>
   );
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,9 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
